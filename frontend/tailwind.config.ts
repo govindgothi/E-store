c00b997c2aa5a7898ba522a57de9770676e2e119
govindgothi <govindgothi5815@gmail.com>
2025-04-16 13:37:13 +0530

    set all config

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+
+
+export default {
+    content: [
+      "./index.html",
+        "./src/**/*.{html,ts,tsx}",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+            primary:{
+                yellow: '#F8C027', // Replace with your custom color     
+                blue:'#00ff' 
+            },
+            fontFamily: {
+                nunito: ['Nunito', 'sans-serif'],
+                sans: ['"Merriweather Sans"', 'sans-serif'],
+              },
+        },
+        screens: {
+          'xp':'340px',
+          'xt':'415px',
+          'xs': '480px', // Example: custom "xs" breakpoint
+          'xr': '580px',
+          'ms':'910px'
+        },
+      },
+    },
+    plugins: [],
+  }
